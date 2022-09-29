ab4d8d39a502f70dd454989ee8576bafe5a57476
hypoxis <64903997+hypoxis@users.noreply.github.com>
2022-09-29 12:35:34 +0300

    Update script.js

--- Assignments/module-4/script.js
+++ Assignments/module-4/script.js
@@ -28,22 +28,27 @@ WARNING!!! WARNING!!!
 */
 
 
-(function () {
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names =new Array();
+names[0]="Yaakov";
+names[1]="John";
+names[2]="Jen";
+names[3]="Jason";
+names[4]="Paul";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="Jim";
 
 for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  if (firstLetter === 'j') {
-    byeSpeaker(names[i]);
+  if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j'){
+    console.log("Goodbye "+ names[i])
   }
-  else {
-    helloSpeaker(names[i]);
+  else{
+    console.log("Hello "+ names[i])
   }
 }
 
-})();
 
 
 
